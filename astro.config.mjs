90df942dd5d6e602f76545207a7b2d42ee7173aa
Juan J Rebollo <jj.rebollo.barranco@gmail.com>
2026-05-08 22:07:35 +0200

    Add production site URL for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://jrebollo.dev',
 	i18n: {
 		defaultLocale: 'en',
 		locales: ['en', 'es', 'pt'],
